fcf3816d30caad4cb0e424e9c4dae05fc1853f53
Rdelys <rdauphinelys@gmail.com>
2026-02-23 13:44:04 +0300

    string in text

--- database/migrations/2026_02_23_103759_alter_website_column_in_google_places_table.php
+++ database/migrations/2026_02_23_103759_alter_website_column_in_google_places_table.php
@@ -3,19 +3,18 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration {
+
     public function up(): void
     {
-        Schema::table('google_places', function (Blueprint $table) {
-            $table->text('website')->change();
-        });
+        // Version ultra safe en prod
+        DB::statement('ALTER TABLE google_places MODIFY website TEXT NULL');
     }
 
     public function down(): void
     {
-        Schema::table('google_places', function (Blueprint $table) {
-            $table->string('website', 255)->change();
-        });
+        DB::statement('ALTER TABLE google_places MODIFY website VARCHAR(255) NULL');
     }
 };
